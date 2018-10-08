feb0a5bd17420fd5303940fef017b68277239bfc
Shawn <da_capo_usa@hotmail.com>
2018-10-08 13:33:23 -0400

    bug fix

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-import urllib3
-import json
+'''
+Tha main program that read local json file, and implement Google translation API to translate words into target language
+'''
+
 import scripts.translation as trans
 from google.cloud import translate
 import json
 
+# Instantiates a client
 translate_client = translate.Client()
+trans_res = {}
 
+# Read local json file
 with open('translations.json') as translation_file:
     translation_content = json.load(translation_file)
 
-trans_res = {}
+# Implement translation function
 for target, content in translation_content.items():
     trans_res[target] = {}
     for text, trans_content in content.items():
         if trans_content != '':
             trans_res[target][text] = trans_content
             continue
-        res = trans.translation(translate_client, text, target.split('-')[0])
+        try:
+            res = trans.translation(translate_client, text, target.split('-')[0])
+        except Exception:
+            print('Error occurs while attempting to translate')
+            print(Exception)
+            break
         trans_res[target][text] = res['translatedText']
 
+# Write translation result into new json file
 with open('translations_after.json', 'w') as outfile:
-    json.dump(trans_res, outfile)
+    json.dump(trans_res, outfile, index=4)
 
-# http = urllib3.PoolManager()
-#
-# data = {'q': 'Address', 'target': 'de', }
-# encoded_data = json.dumps(data).encode('utf-8')
-# r = http.request('POST', 'https://translation.googleapis.com/language/translate/v2', body=encoded_data)
-#
-# print(json.loads(r.data.decode('utf-8')))
--- scripts/translation.py
+++ scripts/translation.py
@@ -1,5 +1,6 @@
+# Translation function with Google Translation API
 def translation(client, text, target):
     res = client.translate(
         text,
-        target_language=target)
-    return res
+        target_language=target, source_language='en')
+    return res['translatedText']
