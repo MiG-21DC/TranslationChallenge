3080afdb641b7edba65082ae0d8108ec447eed5a
Shawn <da_capo_usa@hotmail.com>
2018-10-08 23:36:43 -0400

    Finalize code for version 0.2

--- main.py
+++ main.py
@@ -16,27 +16,10 @@ trans_res = {}
 with open('translations.json') as translation_file:
     translation_content = json.load(translation_file)
 
-trans_res = {}
+# Implement translation function with Translation object
 for target, content in translation_content.items():
     trans = Translations(translate_client, target, content)
     trans_res.update(trans.translated_content)
-# # Implement translation function
-# for target, content in translation_content.items():
-#     trans_res[target] = {}
-#     for text, trans_content in content.items():
-#         if trans_content != '':
-#             trans_res[target][text] = trans_content
-#             continue
-#         try:
-#             res = trans.translation(translate_client, text, target.split('-')[0])
-#         except Exception:
-#             print('Error occurs while attempting to translate')
-#             print(Exception)
-#             break
-#         trans_res[target][text] = res
-
-# Implement translation function with Translation object
-
 
 # Write translation result into new json file
 with open('translations_after.json', 'w') as outfile:
