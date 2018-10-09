a6722ce5b6607dae4c49e2d4c1a2089c949b61c0
Shawn <da_capo_usa@hotmail.com>
2018-10-08 23:15:25 -0400

    Version 2.0. Test new method to approach translation results

--- main.py
+++ main.py
@@ -2,7 +2,8 @@
 Tha main program that read local json file, and implement Google translation API to translate words into target language
 '''
 
-import scripts.translation as trans
+# import scripts.translation as trans
+from scripts.translation import Translations
 from google.cloud import translate
 import json
 
@@ -10,24 +11,32 @@ import json
 translate_client = translate.Client()
 trans_res = {}
 
+
 # Read local json file
 with open('translations.json') as translation_file:
     translation_content = json.load(translation_file)
 
-# Implement translation function
+trans_res = {}
 for target, content in translation_content.items():
-    trans_res[target] = {}
-    for text, trans_content in content.items():
-        if trans_content != '':
-            trans_res[target][text] = trans_content
-            continue
-        try:
-            res = trans.translation(translate_client, text, target.split('-')[0])
-        except Exception:
-            print('Error occurs while attempting to translate')
-            print(Exception)
-            break
-        trans_res[target][text] = res
+    trans = Translations(translate_client, target, content)
+    trans_res.update({target, trans.translated_content})
+# # Implement translation function
+# for target, content in translation_content.items():
+#     trans_res[target] = {}
+#     for text, trans_content in content.items():
+#         if trans_content != '':
+#             trans_res[target][text] = trans_content
+#             continue
+#         try:
+#             res = trans.translation(translate_client, text, target.split('-')[0])
+#         except Exception:
+#             print('Error occurs while attempting to translate')
+#             print(Exception)
+#             break
+#         trans_res[target][text] = res
+
+# Implement translation function with Translation object
+
 
 # Write translation result into new json file
 with open('translations_after.json', 'w') as outfile:
--- scripts/translation.py
+++ scripts/translation.py
@@ -1,6 +1,33 @@
 # Translation function with Google Translation API
-def translation(client, text, target):
-    res = client.translate(
-        text,
-        target_language=target, source_language='en')
-    return res['translatedText']
+# def translation(client, text, target):
+#     res = client.translate(
+#         text,
+#         target_language=target, source_language='en')
+#     return res['translatedText']
+
+
+class Translations:
+    def __init__(self, client, target, content):
+        self.target = target
+        self.source = 'en'
+        self.content = content
+        self.client = client
+        self.translated_content = self.translation()
+
+    def translation(self):
+        trans_res = {}
+        trans_res[self.target] = {}
+        for text, trans_text in self.content.items():
+            if trans_text != '':
+                trans_res[self.target][text] = trans_text
+                continue
+            try:
+                res = self.client.translate(
+                    text,
+                    target_language=self.target.split('-')[0], source_language='en')
+            except Exception:
+                print('Error occurs while attempting to translate: {}').format(text)
+                print(Exception)
+                break
+            trans_res[self.target][text] = res['translatedText']
+        return trans_res
